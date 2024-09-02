905d0fa504125c3b0ec7372b528cad74ec59c99b
lihuahua123 <771725652@qq.com>
2024-09-02 20:16:03 +0800

    x

--- benchmark/multi_turn_example.py
+++ benchmark/multi_turn_example.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass, field
 from typing import List, Optional, Union
 import json
 import asyncio
+import jsonlines
 @dataclass
 class RequestFuncInput:
     prompt: str
@@ -16,8 +17,7 @@ class RequestFuncInput:
     model: str
     best_of: int = 1
     use_beam_search: bool = False
-
-
+    need_cache: bool = False
 @dataclass
 class RequestFuncOutput:
     generated_text: str = ""
@@ -50,15 +50,15 @@ api_url = "http://localhost:30000/v1/chat/completions"
 assert api_url.endswith(
     "chat/completions"
 ), "OpenAI Chat Completions API URL must end with 'chat/completions'."
-request_func_input = RequestFuncOutput()
-request_func_input.use_beam_search = False
-request_func_input.model="/hy-tmp/"
-request_func_input.prompt=[{
-        "role": "user",
-        "content": user_messages[0],
-    }]
-request_func_input.output_len = 200
-request_func_input.prompt_len = len(request_func_input.prompt)
+async def send_cache_flush_request():
+    async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
+        try:
+            flush_url = "http://localhost:30000/flush_cache"
+            async with session.get(url=flush_url) as response:
+                assert response.status == 200
+        except Exception as e:
+            print(e)
+            
 async def async_request_openai_chat_completions(
     request_func_input: RequestFuncInput
 ) -> RequestFuncOutput:
@@ -69,7 +69,8 @@ async def async_request_openai_chat_completions(
             "messages": request_func_input.prompt,
             "temperature": 0.0,
             "max_tokens": request_func_input.output_len,
-            "stream": True
+            "stream": True,
+            "need_cache": request_func_input.need_cache,
         }
         headers = {
             "Content-Type": "application/json",
@@ -77,7 +78,7 @@ async def async_request_openai_chat_completions(
         }
 
         output = RequestFuncOutput()
-        output.prompt_len = request_func_input.prompt_len
+        output.prompt_len = len(request_func_input.prompt)
 
         generated_text = ""
         ttft = 0.0
@@ -127,7 +128,56 @@ async def async_request_openai_chat_completions(
             print(e)
             output.success = False
     return output
-begin = time.time()       
-asyncio.run(async_request_openai_chat_completions(request_func_input=request_func_input))
+
+
+async def client(message_hostory_data):
+    request_func_input = RequestFuncOutput()
+    request_func_input.use_beam_search = False
+    request_func_input.model="/hy-tmp/"
+    request_func_input.output_len = 100
+    request_func_input.prompt = []
+    for index, message in enumerate(message_hostory_data):
+        if sum([len(p["content"]) for p in request_func_input.prompt]) + len(message) + request_func_input.output_len > 2000:
+            break
+        if index < 5:
+            request_func_input.need_cache = True
+        else:
+            request_func_input.need_cache = True
+        request_func_input.prompt.append({
+            "role": "user",
+            "content": message,
+        })
+        result = await async_request_openai_chat_completions(request_func_input=request_func_input)
+        request_func_input.prompt.append({
+            "role": "assistant",
+            "content": result.generated_text
+        })
+    print("conversation turns:", index)
+async def test():
+    dataset_path = "/root/jointserve/benchmark/sharegpt_gpt4.jsonl"
+    dataset = []
+    message_hostory_dataset = []
+    with open(dataset_path, "r+", encoding="utf8") as f:
+        for item in jsonlines.Reader(f):
+            dataset.append(item)
+    for data in dataset:
+        conversation_len = len(data["conversations"])
+        message_history = []
+        if  conversation_len==0 or conversation_len % 2 !=0 :
+            continue
+        for i in range(0,conversation_len,2):
+            message_history.append(data["conversations"][i]["value"])
+        message_hostory_dataset.append(message_history)
+    client_tasks = []
+    for message_hostory_data in message_hostory_dataset[:20]:
+        print("dispatch")
+        client_tasks.append(asyncio.create_task(client(message_hostory_data)))
+    for task in client_tasks:
+        await task
+    
+    
+begin = time.time()
+asyncio.run(send_cache_flush_request())       
+asyncio.run(test())
 end = time.time()
 print(end - begin)
